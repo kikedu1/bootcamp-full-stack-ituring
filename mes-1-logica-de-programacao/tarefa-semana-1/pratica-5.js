31e87cafc8061fed18c99e65933808fc0a212779
Enrique Duran <enriduranh@gmail.com>
2022-10-10 20:20:24 -0300

    mensagem de WhatsApp com JS

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,11 @@
+let nome 
+let email
+let profissao
+let idade
+let lugar_sonho
+nome='Enrique'
+email='enriduranh@gmail.com'
+profissao='Engenheiro de produção'
+idade=40
+lugar_sonho='Taiti'
+console.log("Olá, "+nome+"! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para "+lugar_sonho+". Confirma pra gente alguns dados? A sua idade é "+idade+", você é "+profissao+" e o seu email é "+email+"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
