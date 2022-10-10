1cb76e5553da9c718490e4d15917a1883316fc69
Enrique Duran <enriduranh@gmail.com>
2022-10-10 20:03:55 -0300

    verificando se é uma string ou não

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,15 @@
+let nome 
+let email
+let profissao
+let idade
+let lugar_sonho
+nome='Enrique'
+email='enriduranh@gmail.com'
+profissao='Engenheiro de produção'
+idade=40
+lugar_sonho='Taiti'
+console.log(typeof(nome) == "string")
+console.log(typeof(email) == "string")
+console.log(typeof(profissao) == "string")
+console.log(typeof(idade) == "string")
+console.log(typeof(lugar_sonho) == "string")
