ced3bd5c8aa9870fbeefe65614762495018587e1
Enrique Duran <enriduranh@gmail.com>
2022-10-18 17:15:56 -0300

    valor com desconto

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-4.js
@@ -0,0 +1,7 @@
+function calculaDesconto(valor,desconto){
+    const valorFinal = valor*(1-desconto/100);
+    return valorFinal.toFixed(2);
+}
+
+const valorComDesconto = calculaDesconto(1000,20);
+console.log(valorComDesconto);
