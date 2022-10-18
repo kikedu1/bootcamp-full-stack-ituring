637e8eb8b72d65d26f05553ea58b2a4a5908195e
Enrique Duran <enriduranh@gmail.com>
2022-10-18 19:46:58 -0300

    função Par/Impar

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
@@ -0,0 +1,14 @@
+/*
+const numero = 12;
+const ehPar = numero % 2 === 0;
+*/
+
+function parImpar (x){
+if (x % 2 === 0) {
+    console.log(`O número ${x} é par`)
+} else {
+    console.log(`O número ${x} é impar'`)
+}
+}
+
+parImpar(12)
