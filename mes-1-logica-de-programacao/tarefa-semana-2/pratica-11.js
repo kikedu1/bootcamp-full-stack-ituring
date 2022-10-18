03378d01a0d6f834bd7ce65a50767077a86a7a7a
Enrique Duran <enriduranh@gmail.com>
2022-10-18 20:33:07 -0300

    Exibindo a cor RGB

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-11.js
@@ -0,0 +1,36 @@
+function cores(x){
+    switch(x) {
+        case 'vermelha':
+            console.log('rgb(255,0,0)');
+            break;
+            case 'laranja':
+                console.log('rgb(255,128,0)');
+                break;
+                case 'amarela':
+                    console.log('rgb(255,255,0)');
+                    break;
+                    case 'verde':
+                        console.log('rgb(,255,)');
+                        break;
+                        case 'azul claro':
+                            console.log('rgb(0,255,255)');
+                            break;
+                                case 'azul marinho':
+                                console.log('rgb(0,0,255)');
+                                break;
+                                    case 'rosa':
+                                    console.log('rgb(255,0,255)');
+                                    break;
+                                        default:
+                                        console.log('rgb(255,255,255)');
+    }
+}
+
+cores('vermelha')
+cores('laranja')
+cores('amarela')
+cores('verde')
+cores('azul claro')
+cores('azul marinho')
+cores('rosa')
+cores('vinho')
