35913cb185850d4fd75c1f933243f1ea6127e99a
Enrique Duran <enriduranh@gmail.com>
2022-10-18 19:30:42 -0300

    Olá Mundo em diferentes linguas

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-7.js
@@ -0,0 +1,17 @@
+function language(x) {
+    if (x == "es") {
+        console.log('¡Hola, Mundo!');
+    } else if (x == "pt") {
+        console.log('Olá, Mundo!');
+    } else if (x == "en") {
+        console.log('Hello, World!');
+    } else  {
+        console.log('Olá, Mundo!');
+    }
+   
+}
+
+language("es")
+language("pt")
+language("en")
+language("it")
